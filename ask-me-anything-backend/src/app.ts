f367b76523095e3d29e8160f1fbb543abd0b5ed0
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-19 19:44:49 -0300

    feat: creating basic app configs

---
+++ ask-me-anything-backend/src/app.ts
@@ -0,0 +1,33 @@
+import express, { Router } from 'express';
+import connectToDatabase from './connection';
+// import HanldeError from './middlewares/handleError';
+
+class App {
+  public app: express.Application;
+
+  // public handleError = new HanldeError();
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+
+  public startServer(PORT: string | number = 3001): void {
+    connectToDatabase();
+    this.app.listen(
+      PORT,
+      () => console.log(`Server running here 👉 http://localhost:${PORT}`),
+    );
+  }
+
+  public addRouter(router: Router) {
+    this.app.use(router);
+    // this.app.use(this.handleError.genericError);
+  }
+
+  public getApp() {
+    return this.app;
+  }
+}
+
+export default App;
