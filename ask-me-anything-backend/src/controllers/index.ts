2e890b7cb2feefd179a8efba2988986a418bb4a0
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-19 19:56:12 -0300

    feat: create controller abstract class and RequestWithBodyI interface

---
+++ ask-me-anything-backend/src/controllers/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { RequestWithBody } from '../interfaces/RequestWithBodyI';
+
+import Service from '../services';
+
+abstract class Controller<T> {
+  abstract route: string;
+
+  constructor(public service: Service<T>) {}
+
+  abstract create(req: RequestWithBody<T>, res: Response<T | Error>):
+  Promise<typeof res>;
+
+  abstract read(req: Request, res: Response<T[] | Error>): Promise<typeof res>;
+
+  abstract readOne(req: Request, res: Response<T | Error>): Promise<typeof res>;
+
+  abstract update(req: RequestWithBody<T>, res: Response<T | Error>):
+  Promise<typeof res>;
+
+  abstract delete(req: Request, res: Response<[] | Error>): Promise<typeof res>;
+}
+
+export default Controller;
