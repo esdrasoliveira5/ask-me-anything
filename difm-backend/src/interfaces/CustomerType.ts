00cc792bf2ffdc442048c56ea0e03e0ee3ee9178
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 19:17:43 -0300

    feat: implemented CustomerType

---
+++ difm-backend/src/interfaces/CustomerType.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'; 
+import { User } from './UserType';
+import { WorkerSchema } from './WorkerType';
+
+const CustomerSchema = z.object({
+  hires: z.array(WorkerSchema),
+});
+
+export type Customer = User & z.infer<typeof CustomerSchema>;
+export { CustomerSchema };
