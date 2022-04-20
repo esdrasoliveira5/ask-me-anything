e5b30cf49c7b6bd7fc8b54e08a6ac8057a1d9a87
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 20:54:59 -0300

    feat: implemented CustomService and CustomService method create

---
+++ difm-backend/src/services/CustomerService.ts
@@ -0,0 +1,27 @@
+import Service from '.';
+import { Customer } from '../interfaces/CustomerType';
+import {
+  ResponseCreate,
+  ResponseError,
+} from '../interfaces/ResponsesInterface';
+import CustomerModel from '../models/CustomerModel';
+
+class CustomerService extends Service<Customer> {
+  constructor(model = new CustomerModel()) {
+    super(model);
+  }
+
+  create = async (obj:Customer):
+  Promise<ResponseCreate<Customer> | ResponseError> => {
+    const response = await this.model.create({ ...obj, hires: [] });
+    if (response === undefined) {
+      return {
+        status: 500,
+        response: { error: 'Internal Server Error' },
+      };
+    }
+    return { status: 201, response };
+  };
+}
+
+export default CustomerService;
