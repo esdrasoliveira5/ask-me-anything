2f8aaea588232fddb7733a7a6f61f76015489478
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 20:08:03 -0300

    feat: implemented CustomerModel in Models

---
+++ difm-backend/src/models/CustomerModel.ts
@@ -0,0 +1,27 @@
+import { Schema, model as createModel, Document } from 'mongoose';
+import { Customer } from '../interfaces/CustomerType';
+import MongoModel from './MongoModel';
+
+interface CustomerDocument extends Customer, Document {}
+
+const customerSchema = new Schema<CustomerDocument>(
+  {
+    name: String,
+    lastName: String,
+    email: String,
+    contact: String,
+    password: String,
+    type: String,
+    hires: Array,
+    address: Object,
+  },
+  { versionKey: false },
+);
+
+class CustomerModel extends MongoModel<Customer> {
+  constructor(model = createModel('customer', customerSchema)) {
+    super(model);
+  }
+}
+
+export default CustomerModel;
