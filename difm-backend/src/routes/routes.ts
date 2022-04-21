0e20b48759905232caf2f91044d6ede3457a7df5
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 22:25:26 -0300

    refactor: remove unused routes in routes.ts

--- difm-backend/src/routes/routes.ts
+++ difm-backend/src/routes/routes.ts
@@ -13,10 +13,6 @@ class CustomRouter<T> {
     route: string = controller.route,
   ) {
     this.router.post(route, controller.create);
-    this.router.get(route, controller.read);
-    this.router.get(`${route}/:id`, controller.readOne);
-    this.router.put(`${route}/:id`, controller.update);
-    this.router.delete(`${route}/:id`, controller.delete);
   }
 }
 
