7ff0aa3f079239528e2391110884b575df86f89a
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-21 18:14:15 -0300

    test: add unit test CustomService read

--- difm-backend/src/tests/unit/services/CustomerService.test.ts
+++ difm-backend/src/tests/unit/services/CustomerService.test.ts
@@ -37,7 +37,7 @@ describe('3 - Test customerServices', () => {
         sinon.restore();
       })
     
-      it('return a object with status 201 and the customer created in the db', async () => {
+      it('a.1) return a object with status 201 and the customer created in the db', async () => {
         const response = await customer.create({
           name: 'Roberto',
           lastName: 'Oliveira',
@@ -68,7 +68,7 @@ describe('3 - Test customerServices', () => {
         sinon.restore();
       })
     
-      it('return an object with status 500 and an error message "Internal Server Error"', async () => {
+      it('b.1) return an object with status 500 and an error message "Internal Server Error"', async () => {
         const response = await customer.create({
           name: 'Roberto',
           lastName: 'Oliveira',
@@ -88,7 +88,7 @@ describe('3 - Test customerServices', () => {
         expect(response).to.be.deep.equal({ status: 500, response: { error: 'Internal Server Error'} });
       });
 
-      it('return an object with status 400 and an error message "name is required"', async () => {
+      it('b.2) return an object with status 400 and an error message "name is required"', async () => {
         const response = await customer.create({
           lastName: 'Oliveira',
           email: 'roberto@email.com',
@@ -106,6 +106,62 @@ describe('3 - Test customerServices', () => {
         } as Customer)
         
         expect(response.status).to.be.equal(400);
+      });
+    });
+  });
+  describe('3.2 - method read', () => {
+    describe('a) if success', () => {
+      const payload = [
+        {
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '$2b$10$JOmGDGptDGC1.eLa3OMj0uAk4FxZT2SjLH0lbP3Uh9W7iDHGN3Lp6',
+          type: 'customer',
+          hires: [],
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          }
+        }
+      ];
+
+      before(async () => {
+        sinon
+          .stub(customer.model, 'read')
+          .resolves(payload);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return a array with status 200 and the customers in the db', async () => {
+        const response = await customer.read();
+
+        expect(response).to.be.deep.equal({ status: 200, response: payload });
+      });
+    });
+    describe('b) if fail', () => {
+      before(async () => {
+        sinon
+          .stub(customer.model, 'read')
+          .resolves(undefined);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return an object with status 500 and an error message "Internal Server Error"', async () => {
+        const response = await customer.read();
+
+        expect(response).to.be.deep.equal({ status: 500, response: { error: 'Internal Server Error'} });
       });
     });
   });
