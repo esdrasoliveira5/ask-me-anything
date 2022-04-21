030bd778c7b11659271a6598fe1326bb85cd35b9
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 21:55:24 -0300

    feat: implemented RequestWithBodyInterface and abstract class Controller

--- difm-backend/src/controllers/index.ts
+++ difm-backend/src/controllers/index.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { RequestWithBody } from '../interfaces/RequestWithBodyI';
 
 import Service from '../services';
@@ -10,15 +10,6 @@ abstract class Controller<T> {
 
   abstract create(req: RequestWithBody<T>, res: Response<T | Error>):
   Promise<typeof res>;
-
-  abstract read(req: Request, res: Response<T[] | Error>): Promise<typeof res>;
-
-  abstract readOne(req: Request, res: Response<T | Error>): Promise<typeof res>;
-
-  abstract update(req: RequestWithBody<T>, res: Response<T | Error>):
-  Promise<typeof res>;
-
-  abstract delete(req: Request, res: Response<[] | Error>): Promise<typeof res>;
 }
 
 export default Controller;
