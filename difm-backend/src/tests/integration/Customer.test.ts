93833d0b0bf4ba02ab0eae8cc39a8cb5f6802dd1
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-21 01:00:20 -0300

    test: add integration test fail b) status 400

--- difm-backend/src/tests/integration/Customer.test.ts
+++ difm-backend/src/tests/integration/Customer.test.ts
@@ -14,35 +14,33 @@ const { expect } = chai;
 
 describe('1 - Test endpoint POST /customer', () => {
   describe('1.1 - if success', () => {
-    let chaiHttpResponse: Response;
-    const customerPayload: Customer = {
-      name: 'Roberto',
-      lastName: 'Oliveira',
-      email: 'roberto@email.com',
-      contact: '11987654321',
-      password: '123456789',
-      type: 'customer',
-      hires: [],
-      address: {
-        street: 'avenida',
-        number: '100A',
-        district: 'Bairro',
-        zipcode: '45687-899',
-        city: 'cidade',
-        state: 'estado'
+    it('a) return status 201 and the user created', async () => {
+      let chaiHttpResponse: Response;
+      const customerPayload: Customer = {
+        name: 'Roberto',
+        lastName: 'Oliveira',
+        email: 'roberto@email.com',
+        contact: '11987654321',
+        password: '123456789',
+        type: 'customer',
+        hires: [],
+        address: {
+          street: 'avenida',
+          number: '100A',
+          district: 'Bairro',
+          zipcode: '45687-899',
+          city: 'cidade',
+          state: 'estado'
+        }
       }
-    }
-
-    before(() => {
-      sinon
-      .stub(customer.model, 'create')
-      .resolves(customerPayload);
-    });
-    after(()=>{
-      sinon.restore();
-    });
-
-    it('A) return status 201 and the user created', async () => {
+      before(() => {
+        sinon
+        .stub(customer.model, 'create')
+        .resolves(customerPayload);
+      });
+      after(()=>{
+        sinon.restore();
+      });
       chaiHttpResponse = await chai
          .request(server.app)
          .post('/customer')
@@ -63,7 +61,6 @@ describe('1 - Test endpoint POST /customer', () => {
               "state": "estado"
           }
       });
-      
       expect(chaiHttpResponse).to.have.status(201);
       expect(chaiHttpResponse.body).to.deep.equal({
         "name": "Roberto",
@@ -85,18 +82,16 @@ describe('1 - Test endpoint POST /customer', () => {
     });
   });
   describe('1.1 - if fail', () => {
-    let chaiHttpResponse: Response;
-
-    before(() => {
-      sinon
-      .stub(customer.model, 'create')
-      .resolves(undefined);
-    });
-    after(()=>{
-      sinon.restore();
-    });
-
-    it('A) return status 500 and the error message "Internal Server Error"', async () => {
+    it('a) return status 500 and the error message "Internal Server Error"', async () => {
+      let chaiHttpResponse: Response;
+      before(() => {
+        sinon
+        .stub(customer.model, 'create')
+        .resolves(undefined);
+      });
+      after(()=>{
+        sinon.restore();
+      });
       chaiHttpResponse = await chai
          .request(server.app)
          .post('/customer')
@@ -117,9 +112,32 @@ describe('1 - Test endpoint POST /customer', () => {
               "state": "estado"
           }
       });
-      
       expect(chaiHttpResponse).to.have.status(500);
       expect(chaiHttpResponse.body).to.deep.equal({ "error": "Internal Server Error"});
+    });
+    it('b) return status 400 and the error message "name is required "', async () => {
+      let chaiHttpResponse: Response;
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .post('/customer')
+         .set('X-API-Key', 'foobar')
+         .send({
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "11987654321",
+          "password": "123456789",
+          "type": "customer",
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado"
+          }
+      });
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body).to.deep.equal({ "error": "name is required"});
     });
   });
 });
