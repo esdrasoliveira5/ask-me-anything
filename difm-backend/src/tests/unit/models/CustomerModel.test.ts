9914bf0c17208d119aaa2ac294559faeb11df2b5
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-21 17:59:21 -0300

    test: add unit test CustomerMOdel read

--- difm-backend/src/tests/unit/models/CustomerModel.test.ts
+++ difm-backend/src/tests/unit/models/CustomerModel.test.ts
@@ -90,4 +90,59 @@ describe('2 - Test customerModel', () => {
       });
     });
   });
+  describe('2.2 - method read', () => {
+    describe('a) if success', () => {
+      const payload = [
+        {
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '123456789',
+          type: 'customer',
+          hires: [],
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          }
+        }
+      ];
+
+      before(async () => {
+        sinon
+          .stub(customer.model, 'find')
+          .resolves(payload);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return the user created in the db', async () => {
+        const response = await customer.read()
+        expect(response).to.be.deep.equal(payload);
+      });
+    });
+    describe('b) if fail', () => {
+      before(async () => {
+        sinon
+          .stub(customer.model, 'create')
+          .resolves(undefined);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return undefined', async () => {
+        const response = await customer.read()
+  
+        expect(response).to.be.equal(undefined);
+      });
+    });
+  });
 });
