9d5baf6b8132893db6e2d1633a740f5c6576abc6
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-20 21:49:46 -0300

    test: add test CustomerController create

---
+++ difm-backend/src/tests/unit/controllers/CustomerController.test.ts
@@ -0,0 +1,134 @@
+import { Request, Response } from 'express';
+import * as sinon from 'sinon';
+import chai from 'chai';
+
+
+const { expect } = chai;
+const customer = new CustomerController();
+const request = {} as Request;
+const response = {} as Response;
+
+describe('3 - Test CustomerController', () => {
+  describe('3.1 - method create', () => {
+    describe('if success', () => {
+      const payload = {
+        status: 201,
+        response: {
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '123456789',
+          type: 'customer',
+          hires: [],
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          }
+        }
+      }
+    
+      before(async () => {
+        request.body = {
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '123456789',
+          type: 'customer',
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          }
+        }
+        response.status = sinon.stub().returns(response)
+        response.json = sinon.stub()
+        
+        sinon
+          .stub(customer.service, 'create')
+          .resolves(payload);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return the status 201 and the user created', async () => {
+        await customer.create(request, response);
+        
+        expect((response.status as sinon.SinonStub).calledWith(201))
+        expect((response.json as sinon.SinonStub).calledWith({
+          "name": 'Roberto',
+          "lastName": 'Oliveira',
+          "email": 'roberto@email.com',
+          "contact": '11987654321',
+          "password": '123456789',
+          "type": 'customer',
+          "hires": [],
+          "address": {
+            "street": 'avenida',
+            "number": '100A',
+            "district": 'Bairro',
+            "zipcode": '45687-899',
+            "city": 'cidade',
+            "state": 'estado'
+          }
+        }))
+      });
+    });
+    describe('if fails', () => {
+      const payload = {
+        status: 400,
+        response: {
+          error: "Error",
+        }
+      }
+    
+      before(async () => {
+        request.body = {
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '123456789',
+          type: 'customer',
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          }
+        }
+        response.status = sinon.stub().returns(response)
+        response.json = sinon.stub()
+        
+        sinon
+          .stub(customer.service, 'create')
+          .resolves(payload);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return the status 500 and error message "Internal Server Error"', async () => {
+        await customer.create(request, response);
+         
+        expect((response.status as sinon.SinonStub).calledWith(500));
+        expect((response.json as sinon.SinonStub).calledWith({
+          "error": "Internal Server Error"
+        }))
+      });
+    });
+  });
+});  
