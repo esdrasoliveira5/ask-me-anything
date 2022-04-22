1b3de969eac4e43837eb678f1076a379e700c369
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-21 21:19:15 -0300

    test: add unit test CustomerModel readOne

--- difm-backend/src/tests/unit/models/CustomerModel.test.ts
+++ difm-backend/src/tests/unit/models/CustomerModel.test.ts
@@ -4,27 +4,27 @@ import CustomerModel from '../../../models/CustomerModel';
 
 const customer = new CustomerModel();
 const { expect } = chai;
+const payload = {
+  name: 'Roberto',
+  lastName: 'Oliveira',
+  email: 'roberto@email.com',
+  contact: '11987654321',
+  password: '123456789',
+  type: 'customer',
+  hires: [],
+  address: {
+    street: 'avenida',
+    number: '100A',
+    district: 'Bairro',
+    zipcode: '45687-899',
+    city: 'cidade',
+    state: 'estado'
+  }
+}
 
 describe('2 - Test customerModel', () => {
   describe('2.1 - method create', () => {
     describe('a) if success', () => {
-      const payload = {
-        name: 'Roberto',
-        lastName: 'Oliveira',
-        email: 'roberto@email.com',
-        contact: '11987654321',
-        password: '123456789',
-        type: 'customer',
-        hires: [],
-        address: {
-          street: 'avenida',
-          number: '100A',
-          district: 'Bairro',
-          zipcode: '45687-899',
-          city: 'cidade',
-          state: 'estado'
-        }
-      }
       before(async () => {
         sinon
           .stub(customer.model, 'create')
@@ -35,7 +35,7 @@ describe('2 - Test customerModel', () => {
         sinon.restore();
       })
     
-      it('return the user created in the db', async () => {
+      it('return the customer created in the db', async () => {
         const response = await customer.create({
           name: 'Roberto',
           lastName: 'Oliveira',
@@ -92,39 +92,19 @@ describe('2 - Test customerModel', () => {
   });
   describe('2.2 - method read', () => {
     describe('a) if success', () => {
-      const payload = [
-        {
-          name: 'Roberto',
-          lastName: 'Oliveira',
-          email: 'roberto@email.com',
-          contact: '11987654321',
-          password: '123456789',
-          type: 'customer',
-          hires: [],
-          address: {
-            street: 'avenida',
-            number: '100A',
-            district: 'Bairro',
-            zipcode: '45687-899',
-            city: 'cidade',
-            state: 'estado'
-          }
-        }
-      ];
-
       before(async () => {
         sinon
           .stub(customer.model, 'find')
-          .resolves(payload);
+          .resolves([payload]);
       });
     
       after(()=>{
         sinon.restore();
       })
     
-      it('return the user created in the db', async () => {
+      it('return the customers in the db', async () => {
         const response = await customer.read()
-        expect(response).to.be.deep.equal(payload);
+        expect(response).to.be.deep.equal([payload]);
       });
     });
     describe('b) if fail', () => {
@@ -142,6 +122,59 @@ describe('2 - Test customerModel', () => {
         const response = await customer.read()
   
         expect(response).to.be.equal(undefined);
+      });
+    });
+  });
+  describe('2.3 - method readOne', () => {
+    describe('a) if success', () => {
+
+      before(async () => {
+        sinon
+          .stub(customer.model, 'findOne')
+          .resolves(payload);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return the customer in the db', async () => {
+        const response = await customer.readOne({
+          name: 'Roberto',
+          lastName: 'Oliveira',
+          email: 'roberto@email.com',
+          contact: '11987654321',
+          password: '123456789',
+          type: 'customer',
+          hires: [],
+          address: {
+            street: 'avenida',
+            number: '100A',
+            district: 'Bairro',
+            zipcode: '45687-899',
+            city: 'cidade',
+            state: 'estado'
+          },
+        });
+
+        expect(response).to.be.deep.equal(payload);
+      });
+    });
+    describe('b) if fail', () => {
+      before(async () => {
+        sinon
+          .stub(customer.model, 'findOne')
+          .resolves(null);
+      });
+    
+      after(()=>{
+        sinon.restore();
+      })
+    
+      it('return undefined', async () => {
+        const response = await customer.readOne(payload)
+  
+        expect(response).to.be.equal(null);
       });
     });
   });
